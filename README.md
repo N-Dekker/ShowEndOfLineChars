148f64ab62a22b3b6fd75c1440d1f90377a4d123
Niels Dekker <N.Dekker@lumc.nl>
2022-06-08 15:20:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ShowEndOfLineChars
---
+++ ShowEndOfLineChars.cpp
@@ -0,0 +1,24 @@
+﻿// Related to https://github.com/isocpp/CppCoreGuidelines/pull/1920
+// -- Niels Dekker, LKEB, Leiden University Medical Center, 2022
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+  const auto fileName = "file.txt";
+  {
+    std::ofstream outputFileStream(fileName);
+
+    for (int i{}; i < 8; ++i)
+    {
+      outputFileStream << i << '\n';
+    }
+  }
+  std::ifstream inputFileStream(fileName, std::ios_base::binary);
+
+  for(const int c: std::string(std::istreambuf_iterator<char>{inputFileStream}, {}))
+  {
+    std::cout << "0x" << std::uppercase << std::hex << c << ' ';
+  }
+}
